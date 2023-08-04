443383dd501798f3dc306a32bf61c02bf72749fb
mpluiz <marcospaulo.luiz@gmail.com>
2023-08-04 18:10:36 -0300

    chore: update peerDependencies

--- packages/eslint-config/node/node.js
+++ packages/eslint-config/node/node.js
@@ -3,26 +3,14 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: ['standard', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint'],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 80,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
-        semi: false,
-      },
-    ],
-  },
+  rules: {},
   settings: {
     'import/parsers': {
       [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
